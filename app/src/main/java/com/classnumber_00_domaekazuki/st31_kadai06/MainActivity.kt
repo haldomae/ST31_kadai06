8cf4879799822f4907da868c9ab620e5ef5438ac
haldomae <hal.domae@gmail.com>
2025-08-31 09:26:41 +0900

    リストの項目を修正

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
@@ -25,11 +25,11 @@ class MainActivity : AppCompatActivity() {
         val listView: ListView = findViewById(R.id.listView)
         // 果物データのリストを作成（Map形式）
         val ListData = arrayListOf(
-            mapOf("name" to "モンスター1", "image" to R.drawable.img1),
-            mapOf("name" to "モンスター2", "image" to R.drawable.img2),
-            mapOf("name" to "モンスター3", "image" to R.drawable.img3),
-            mapOf("name" to "モンスター4", "image" to R.drawable.img4),
-            mapOf("name" to "モンスター5", "image" to R.drawable.img5),
+            mapOf("name" to "モンスター1", "image" to R.drawable.img1,"from" to "HAL東京30F"),
+            mapOf("name" to "モンスター2", "image" to R.drawable.img2,"from" to "HAL東京29F"),
+            mapOf("name" to "モンスター3", "image" to R.drawable.img3,"from" to "HAL東京28F"),
+            mapOf("name" to "モンスター4", "image" to R.drawable.img4,"from" to "HAL東京27F"),
+            mapOf("name" to "モンスター5", "image" to R.drawable.img5,"from" to "HAL東京26F"),
         )
 
         // ArrayAdapterを作成
@@ -37,8 +37,8 @@ class MainActivity : AppCompatActivity() {
             this,// コンテキスト
             ListData,
             R.layout.list_item,     // 標準のリスト項目レイアウト
-            arrayOf("image", "name"),              // データのキー
-            intArrayOf(R.id.itemImage, R.id.itemText)  // 対応するView ID
+            arrayOf("image", "name", "from"),              // データのキー
+            intArrayOf(R.id.itemImage, R.id.itemText, R.id.itemText2)  // 対応するView ID
         )
 
         // ListViewにAdapterを設定
