3ea0978c05fc7748cfaf9953819f8146412cec9b
haldomae <hal.domae@gmail.com>
2025-09-01 16:35:44 +0900

    リスト項目の内容を変更

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
@@ -36,17 +36,23 @@ class MainActivity : AppCompatActivity() {
         // 画像付きデータ
         val monsterData = arrayListOf(
             mapOf("name" to "モンスター1",
-                "image" to R.drawable.img1),
+                "image" to R.drawable.img1,
+                "from" to "HAL東京30F"),
             mapOf("name" to "モンスター2",
-                "image" to R.drawable.img2),
+                "image" to R.drawable.img2,
+                "from" to "HAL東京29F"),
             mapOf("name" to "モンスター3",
-                "image" to R.drawable.img3),
+                "image" to R.drawable.img3,
+                "from" to "HAL東京28F"),
             mapOf("name" to "モンスター4",
-                "image" to R.drawable.img4),
+                "image" to R.drawable.img4,
+                "from" to "HAL東京27F"),
             mapOf("name" to "モンスター5",
-                "image" to R.drawable.img5),
+                "image" to R.drawable.img5,
+                "from" to "HAL東京26F"),
             mapOf("name" to "モンスター6",
-                "image" to R.drawable.img6),
+                "image" to R.drawable.img6,
+                "from" to "HAL東京25F"),
         )
 
         // Adapterを作成
@@ -60,8 +66,8 @@ class MainActivity : AppCompatActivity() {
             this, // コンテキスト
             monsterData, // 表示したいデータ
             R.layout.list_item, // リストのレイアウト
-            arrayOf("image","name"),// 表示するデータのキー
-            intArrayOf(R.id.itemImage, R.id.itemText) // 対応するViewのID
+            arrayOf("image","name", "from"),// 表示するデータのキー
+            intArrayOf(R.id.itemImage, R.id.itemText, R.id.itemText2) // 対応するViewのID
         )
         // Adapterを設定
         list.adapter = adapter
