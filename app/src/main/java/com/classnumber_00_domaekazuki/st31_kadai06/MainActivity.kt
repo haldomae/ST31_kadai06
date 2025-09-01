571484c89d469511cec51d00dcba3db364e2867a
haldomae <hal.domae@gmail.com>
2025-09-01 15:48:26 +0900

    画像付きのリストに変更

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
@@ -3,6 +3,7 @@ package com.classnumber_00_domaekazuki.st31_kadai06
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -23,33 +24,56 @@ class MainActivity : AppCompatActivity() {
         val list: ListView = findViewById(R.id.listview)
 
         // 表示するデータ
-        val fruits = arrayOf(
-            "りんご",
-            "バナナ",
-            "オレンジ",
-            "いちご",
-            "ぶどう",
-            "パイナップル"
+//        val fruits = arrayOf(
+//            "りんご",
+//            "バナナ",
+//            "オレンジ",
+//            "いちご",
+//            "ぶどう",
+//            "パイナップル"
+//        )
+
+        // 画像付きデータ
+        val monsterData = arrayListOf(
+            mapOf("name" to "モンスター1",
+                "image" to R.drawable.img1),
+            mapOf("name" to "モンスター2",
+                "image" to R.drawable.img2),
+            mapOf("name" to "モンスター3",
+                "image" to R.drawable.img3),
+            mapOf("name" to "モンスター4",
+                "image" to R.drawable.img4),
+            mapOf("name" to "モンスター5",
+                "image" to R.drawable.img5),
+            mapOf("name" to "モンスター6",
+                "image" to R.drawable.img6),
         )
 
         // Adapterを作成
         // Adapterはリストとデータを中継してくれるもの
-        val adapter = ArrayAdapter(
+//        val adapter = ArrayAdapter(
+//            this, // コンテキスト
+//            android.R.layout.simple_list_item_1, // 項目のレイアウト
+//            fruits // データ
+//        )
+        val adapter = SimpleAdapter(
             this, // コンテキスト
-            android.R.layout.simple_list_item_1, // 項目のレイアウト
-            fruits // データ
+            monsterData, // 表示したいデータ
+            R.layout.list_item, // リストのレイアウト
+            arrayOf("image","name"),// 表示するデータのキー
+            intArrayOf(R.id.itemImage, R.id.itemText) // 対応するViewのID
         )
         // Adapterを設定
         list.adapter = adapter
 
         // 項目を押した時
         list.setOnItemClickListener{ parent, view, position, id ->
-            val currentData = fruits[position]
-            Toast.makeText(
-                this,
-                currentData,
-                Toast.LENGTH_SHORT
-            ).show()
+//            val currentData = fruits[position]
+//            Toast.makeText(
+//                this,
+//                currentData,
+//                Toast.LENGTH_SHORT
+//            ).show()
         }
     }
 }
