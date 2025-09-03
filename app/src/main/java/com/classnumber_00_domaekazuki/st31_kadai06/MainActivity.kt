1a645ff2d4a494c05cc803c7c50ad851d8fad3d6
haldomae <hal.domae@gmail.com>
2025-09-03 10:04:19 +0900

    リストにデータを表示

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai06/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.classnumber_00_domaekazuki.st31_kadai06
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // ListViewの取得
+        val list: ListView = findViewById(R.id.listview)
+
+        // 表示するデータ
+        val fruits = arrayOf(
+            "りんご",
+            "バナナ",
+            "オレンジ",
+            "いちご",
+            "ぶどう",
+            "パイナップル",
+        )
+
+        // Adapter
+        // Adapterはリストとデータを仲介してくれるもの
+        val adapter = ArrayAdapter(
+            this, // コンテキスト(画面情報、どこの画面に表示するか)
+            android.R.layout.simple_list_item_1, // 項目のレイアウト
+            fruits // 表示するデータ
+        )
+
+        // リストにAdapterを設定
+        list.adapter = adapter
     }
 }
